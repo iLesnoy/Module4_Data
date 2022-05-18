4aefc65ae4629957755bc3e193a0eb31d8073d0a
Yaraslau <gamasileusbed@gmail.com>
2022-05-18 15:07:27 +0300

    add security

---
+++ service/src/main/java/com/epam/esm/gifts/security/JwtTokenFilter.java
@@ -0,0 +1,41 @@
+package com.epam.esm.gifts.security;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@Component
+public class JwtTokenFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        if (Strings.isNotEmpty(token) && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        try {
+            chain.doFilter(request, response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
