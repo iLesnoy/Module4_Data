11586112b12131780d63b2fc17346601b25cdf97
Yaraslau <gamasileusbed@gmail.com>
2022-05-18 15:08:39 +0300

    add jsonFormat

--- service/src/main/java/com/epam/esm/gifts/dto/GiftCertificateDto.java
+++ service/src/main/java/com/epam/esm/gifts/dto/GiftCertificateDto.java
@@ -1,6 +1,7 @@
 package com.epam.esm.gifts.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
@@ -28,8 +29,10 @@ public class GiftCertificateDto extends RepresentationModel<GiftCertificateDto> 
     private BigDecimal price;
     @JsonProperty("duration")
     private int duration;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @JsonProperty("create-date")
     private LocalDateTime createDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @JsonProperty("last-update-date")
     private LocalDateTime lastUpdateDate;
     @JsonProperty("tags")
