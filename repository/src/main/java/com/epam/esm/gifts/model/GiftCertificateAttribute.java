6b1648b7018b4bea558366a0e6b6293336dae010
Yaraslau <gamasileusbed@gmail.com>
2022-06-07 11:35:24 +0300

    fix

--- repository/src/main/java/com/epam/esm/gifts/model/GiftCertificateAttribute.java
+++ repository/src/main/java/com/epam/esm/gifts/model/GiftCertificateAttribute.java
@@ -10,32 +10,10 @@ import java.util.Objects;
 @Builder
 public class GiftCertificateAttribute {
 
-    private static final String DEFAULT_SORT = "ASC";
-    private static final String DEFAULT_TAG_NAME_CONDITION = "tags.name LIKE % AND ";
-    private static final String DEFAULT_SEARCH_PART_CONDITION = "(gift_certificate.name LIKE %)";
-    private static final String DEFAULT_ORDER_CONDITION = "gift_certificate.id" + DEFAULT_SORT;
-
     private List<String> tagNameList;
     private String searchPart;
     private String orderSort;
     private List<String> sortingFieldList;
 
-    public String buildQueryPartForTagNameList() {
-        return CollectionUtils.isEmpty(tagNameList)
-                ? DEFAULT_TAG_NAME_CONDITION
-                : tagNameList.stream().reduce("", (result, next) -> result + "tags.name LIKE %" + next + "% AND ");
-    }
 
-    public String buildQueryPartForSearchPart() {
-        return Objects.nonNull(searchPart)
-                ? "(gift_certificate.name LIKE CONCAT (%" + searchPart + "%) OR gift_certificate.description LIKE CONCAT (%" + searchPart + "%)"
-                : DEFAULT_SEARCH_PART_CONDITION;
-    }
-
-    public String buildOrderByFields() {
-        orderSort = Objects.nonNull(orderSort) ? orderSort : DEFAULT_SORT;
-        return CollectionUtils.isEmpty(sortingFieldList)
-                ? DEFAULT_ORDER_CONDITION
-                : sortingFieldList.stream().reduce("", (result, next) -> result + next + " " + orderSort);
-    }
 }
