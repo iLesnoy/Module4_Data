fd73caa69cfceaadaf7c73660b399a033486e5d4
Yaraslau <gamasileusbed@gmail.com>
2022-06-24 17:59:51 +0300

    fix/upd test

--- web/src/test/java/com/epam/esm/gifts/controller/UserControllerTest.java
+++ web/src/test/java/com/epam/esm/gifts/controller/UserControllerTest.java
@@ -24,7 +24,7 @@ class UserControllerTest {
                 .when().post("http://localhost:8085/gift_system/api/auth/signup")
                 .then()
                 .log().body()
-                .statusCode(HttpStatus.CONFLICT.value());
+                .statusCode(HttpStatus.CREATED.value());
     }
 
     @Test
