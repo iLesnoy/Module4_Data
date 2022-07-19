9bf21c63ae8d4d398fdb8104e74afb49ca537661
Yaraslau <gamasileusbed@gmail.com>
2022-07-19 11:27:27 +0300

    add swagger doc

--- web/src/main/java/com/epam/esm/gifts/controller/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/gifts/controller/GiftCertificateController.java
@@ -4,6 +4,8 @@ import com.epam.esm.gifts.GiftCertificateService;
 import com.epam.esm.gifts.dto.GiftCertificateAttributeDto;
 import com.epam.esm.gifts.dto.GiftCertificateDto;
 import com.epam.esm.gifts.hateaos.HateoasBuilder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@Api("Controller GiftCertificateController crud operations")
 @RestController
 @RequestMapping("/api/certificates")
 public class GiftCertificateController {
@@ -24,6 +27,7 @@ public class GiftCertificateController {
         this.hateoasBuilder = hateoasBuilder;
     }
 
+    @ApiOperation(value = "GiftCertificateDto", notes = "use giftCertificateDto")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasAuthority('certificates:create')")
